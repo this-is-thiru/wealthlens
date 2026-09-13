c3b53b0ab459d5e38e9320d14351811f50999857
Peramasani Thirumala Yadav <pthirumalayadav@gmail.com>
2026-09-13 17:41:22 +0530

    push nonnull base import

--- backend/src/main/java/com/thiru/wealthlens/auth/config/SecurityAuditorAware.java
+++ backend/src/main/java/com/thiru/wealthlens/auth/config/SecurityAuditorAware.java
@@ -1,13 +1,13 @@
 package com.thiru.wealthlens.auth.config;
 
 
-import java.util.Optional;
-
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class SecurityAuditorAware implements AuditorAware<String> {
